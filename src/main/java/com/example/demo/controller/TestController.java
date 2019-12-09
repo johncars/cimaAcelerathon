512d4d3db71926d443f437d80f03d6dd7270aca3
Lopez Pesantes <dlopezp@intercorp.com.pe>
2019-12-09 18:34:36 -0500

    exec api externa

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/api/v1")
@@ -10,6 +12,9 @@ public class TestController{
     
     @GetMapping("/test")
     public String test(){
-        return "Primer Aplicativo";
+        //return "Primer Aplicativo";
+        RestTemplate rt = new RestTemplate();
+        String result = rt.getForObject("https://ghibliapi.herokuapp.com/films", String.class);
+        return result;
     }
 }
