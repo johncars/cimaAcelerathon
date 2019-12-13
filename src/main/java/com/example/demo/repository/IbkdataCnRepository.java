6a9043dab508416eb7cc0af60b0c44f0ad6a89be
Salvador Huarniz <jsalvadorh@intercorp.com.pe>
2019-12-13 14:47:32 -0500

    ahora si xd

--- src/main/java/com/example/demo/repository/IbkdataCnRepository.java
+++ src/main/java/com/example/demo/repository/IbkdataCnRepository.java
@@ -2,11 +2,11 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.entity.IbkdataCn;
+import com.example.demo.entity.IbkDataCn;
 
 import java.util.List;
 
 @Repository
-public interface IbkdataCnRepository extends JpaRepository<IbkLoanCalendar, Integer> {
+public interface IbkdataCnRepository extends JpaRepository<IbkDataCn, Integer> {
 
 }
