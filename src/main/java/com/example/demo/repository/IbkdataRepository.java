7a47b47d4d8325161aaa4b168dc1f2aaf429e9c7
Salvador Huarniz <jsalvadorh@intercorp.com.pe>
2019-12-13 15:13:56 -0500

    prueba

--- src/main/java/com/example/demo/repository/IbkdataRepository.java
+++ src/main/java/com/example/demo/repository/IbkdataRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface IbkdataRepository extends JpaRepository<Ibkdata, Integer> {
-    @Query("select RUC from ibk_data i where i.FINAL_RATE = i") 
-    List<Ibkdata> findprueba(String i);
+    @Query("select RUC from ibk_data i where i.FINAL_RATE = ?1") 
+    List<Ibkdata> findprueba(String j);
 }
