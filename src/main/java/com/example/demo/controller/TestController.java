fc4e55a4aafa8f6d2c2d25bc30c696c759fc3642
Salvador Huarniz <jsalvadorh@intercorp.com.pe>
2019-12-14 00:30:51 -0500

    logro

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -12,9 +12,16 @@ import com.example.demo.repository.IbkLoanDebtRepository;
 import com.example.demo.entity.IbkLoanDebt;
 import com.example.demo.repository.IbkdataCnRepository;
 import com.example.demo.entity.IbkDataCn;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
+import com.google.auth.oauth2.AccessToken;
 @RestController
 @RequestMapping("/api/v1")
 public class TestController {
@@ -23,15 +30,25 @@ public class TestController {
     IbkdataRepository ibkdataRespository;
 
     @GetMapping("/main")
-    public String main() {
-        List<Ibkdata> list = prueba("25");
-        int n = list.size();
-        String cadena = "";
-        for (int i = 0; i < n; i++) {
-            cadena = cadena + list.get(i).getRuc() + " ";
+    public String main(String json) {
+
+        GoogleCredentials credentials;
+
+        try {
+            credentials = GoogleCredentials.fromStream(new FileInputStream(
+                    "/home/acelerathon_cima_grupo_4/cimaAcelerathon/cimaAcelerathon/src/main/resources/chatbot-grupo-4-xulbcw-b88e26e3a39e.json"));
+            if (credentials.createScopedRequired()) {
+                credentials = credentials
+                        .createScoped(Collections.singletonList("https://www.googleapis.com/auth/dialogflow"));
+            }
+
+            credentials.refreshIfExpired();
+            AccessToken token = credentials.getAccessToken();
+            return token.getTokenValue();
+        } catch (IOException e) {
+            return "Hubo un error";
         }
 
-        return cadena;
     }
 
     @GetMapping("/sql")
